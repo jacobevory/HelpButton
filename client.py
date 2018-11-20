14ed0e29a0704803d4633dbbfba8a17d6069286c
Jacob Evory <jacobevory@gmail.com>
2018-11-20 16:25:26 -0500

    refresh rate bug fix

--- client.py
+++ client.py
@@ -20,7 +20,7 @@ GPIO.setup(BUTTON_LIGHT, GPIO.OUT, initial=OFF)
 
 GPIO.setup([BUTTON_INPUT, SWITCH_INPUT], GPIO.IN, pull_up_down=GPIO.PUD_DOWN)
 
-
+count = 0
 button_active = 0
 switch_active = 1
 send_active = 0
@@ -42,7 +42,6 @@ def sendData():
 	channel.basic_publish(exchange='',
 				  routing_key=queueName,
 				  body=sendBody)
-	time.sleep(0.5)
 	print(sendBody)
 	send_active = 0
 	
@@ -50,20 +49,24 @@ def buttonPressed(self):
 	global button_active
 	global switch_active
 	global send_active
+	global count
 	if switch_active == 1:
 		button_active = 1
+	count = 0
 #		sendData()
 
 def switchFlipped(self):
 	global button_active
 	global switch_active
 	global send_active
+	global count
 	button_active = 0
 	time.sleep(0.1)
 	if GPIO.input(SWITCH_INPUT):
 		switch_active = 1
 	else:
 		switch_active = 0
+	count = 0
 #	sendData()
 	
 	
@@ -71,6 +74,7 @@ GPIO.add_event_detect(BUTTON_INPUT, GPIO.RISING, buttonPressed)
 GPIO.add_event_detect(SWITCH_INPUT, GPIO.BOTH, switchFlipped)
 
 if __name__ == '__main__':
+	global count
 	try:
 		switch_active = GPIO.input(SWITCH_INPUT)
 		channel = connection.channel()
@@ -89,6 +93,8 @@ if __name__ == '__main__':
 				GPIO.output(BUTTON_LIGHT, OFF)	
 			if count % 5 == 0:
 				sendData()
+			if count > 600:
+				switch_active = 1;
 			count = count + 1
 			if count >= 65535:
 				count = 0
